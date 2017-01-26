4c08d6f30222ff35b971ef0222af56f9b2d10120
Jesús Ignacio García <jesusignazio@users.noreply.github.com>
2017-01-26 11:24:54 +0100

    Update panel.php

--- public/panel.php
+++ public/panel.php
@@ -2,7 +2,7 @@
     <div class="col-md-3 left_col menu_fixed">
         <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-                <a href="index.php"><img align="middle" id="logo" src="images/escudo.png"></a>
+                <a href="index.php"><img class="image-resposive" align="middle" id="logo" src="images/escudo.png"></a>
             </div>
 
             <div class="clearfix"></div>
