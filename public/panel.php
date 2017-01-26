9021f111f62cc647f561f2c654045c0dcd387fce
Jesús Ignacio García <jesusignazio@users.noreply.github.com>
2017-01-26 10:57:32 +0100

    Create panel.php

---
+++ public/panel.php
@@ -0,0 +1,176 @@
+<div class="main_container">
+    <div class="col-md-3 left_col menu_fixed">
+        <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+                <a href="index.php"><img class="img-responsive" align="middle" id="logo" src="../images/logo.png"></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile">
+                <div id="profile_pic" class="profile_pic">
+                    <img src="images/photo.png" alt="..." class="img-circle profile_img">
+                </div>
+                <div class="profile_info">
+                    <span>Hola,</span>
+                    <h2>Jesús Ignacio</h2>
+                </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                <div class="menu_section">
+                    <h5>.</h5>
+                    <ul class="nav side-menu">
+                        <li><a><i class="fa fa-home"></i> Inicio <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="index.php">Panel de inicio</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a><i class="fa fa-edit"></i> Libro de cuentas <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="movimientos.php">Movimientos</a>
+                                </li>
+                                <li><a id="menu-pendientes" href="pendientes.php">Pagos pendientes<span style="float:right" id="menu-pendientes-label" class="badge orange"></span></a>
+                                </li>
+                                <li><a href="facturas.php">Facturas</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a><i class="fa fa-users"></i> Miembros <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="castores.php">Castores</a>
+                                </li>
+                                <li><a href="lobatos.php">Lobatos</a>
+                                </li>
+                                <li><a href="rangers.php">Rangers</a>
+                                </li>
+                                <li><a href="pioneros.php">Pioneros</a>
+                                </li>
+                                <li><a href="rutas.php">Rutas</a>
+                                </li>
+                                <li><a href="responsables.php">Responsables</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a><i class="fa fa-tags"></i> Categorías <span class="fa fa-chevron-down"></span></a>
+                            <ul class="nav child_menu">
+                                <li><a href="cuentas.php">Cuentas</a>
+                                </li>
+                                <li><a href="tarjetas.php">Tarjetas</a>
+                                </li>
+                                <li><a href="categorias.php">Categorías de movimientos</a>
+                                </li>
+                                <li><a href="pagos.php">Categorías de pagos</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>  
+        </div>    
+    </div>
+
+    <!-- top navigation -->
+    <div class="top_nav">
+        <div class="nav_menu">
+            <nav>
+                <div class="nav toggle">
+                    <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                </div>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                            <img src="images/photo.png" alt="">Jesús Ignacio
+                            <span class=" fa fa-angle-down"></span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-usermenu pull-right">
+                            <li><a href="login.php"><i class="fa fa-sign-out pull-right"></i> Cerrar sesión</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li role="presentation" class="dropdown">
+                        <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                            <i class="fa fa-envelope-o"></i>
+                            <span class="badge bg-green">6</span>
+                        </a>
+                        <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                            <li>
+                                <a>
+                                    <span class="image">
+                                        <img src="images/photo.png" alt="Profile Image" />
+                                    </span>
+                                    <span>
+                                        <span>John Smith</span>
+                                        <span class="time">3 mins ago</span>
+                                    </span>
+                                    <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                                    <span>
+                                        <span>John Smith</span>
+                                        <span class="time">3 mins ago</span>
+                                    </span>
+                                    <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                                    <span>
+                                        <span>John Smith</span>
+                                        <span class="time">3 mins ago</span>
+                                    </span>
+                                    <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                                    <span>
+                                        <span>John Smith</span>
+                                        <span class="time">3 mins ago</span>
+                                    </span>
+                                    <span class="message">
+                                        Film festivals used to be do-or-die moments for movie makers. They were where...
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="text-center">
+                                    <a href="inbox.html">
+                                        <strong>See All Alerts</strong>
+                                        <i class="fa fa-angle-right"></i>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- /top navigation -->
