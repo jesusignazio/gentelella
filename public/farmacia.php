31e39ba9057614d44ea58629eaf40897787f8269
Jesús Ignacio García <jesusignazio@users.noreply.github.com>
2017-01-31 11:50:22 +0100

    Update farmacia.php

--- public/farmacia.php
+++ public/farmacia.php
@@ -172,11 +172,7 @@
             function rowStyle(row, index) {
         var classes = ['active', 'success', 'info', 'warning', 'danger'];
 
-        if (index % 2 === 0 && index / 2 < classes.length) {
-            return {
-                classes: classes[index / 2]
-            };
-        }
+        alert(row.fecha_caducidad);
         return {};
     }
            
